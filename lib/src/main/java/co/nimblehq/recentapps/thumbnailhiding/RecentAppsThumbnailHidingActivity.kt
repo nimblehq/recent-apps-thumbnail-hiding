2cb1331e53a10357ebfb8d1b93b774198dc075c2
Luong Vo (Lucas) <luong@nimblehq.co>
2021-08-24 23:27:50 +0700

    Reuse enabling FLAG_SECURE from RecentAppsThumbnailHidingListener

--- lib/src/main/java/co/nimblehq/recentapps/thumbnailhiding/RecentAppsThumbnailHidingActivity.kt
+++ lib/src/main/java/co/nimblehq/recentapps/thumbnailhiding/RecentAppsThumbnailHidingActivity.kt
@@ -2,7 +2,6 @@ package co.nimblehq.recentapps.thumbnailhiding
 
 import android.os.Build
 import android.os.Bundle
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class RecentAppsThumbnailHidingActivity : AppCompatActivity(), RecentAppsThumbnailHidingListener {
@@ -12,10 +11,7 @@ abstract class RecentAppsThumbnailHidingActivity : AppCompatActivity(), RecentAp
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (enableSecureFlagOnLowApiDevices && Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_SECURE,
-                WindowManager.LayoutParams.FLAG_SECURE
-            )
+            enableSecureFlag(true)
         }
     }
 }
