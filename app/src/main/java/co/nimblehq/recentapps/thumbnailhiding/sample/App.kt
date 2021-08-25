39ab147f713389e34a5ec333be02b13b47572bf3
Luong Vo (Lucas) <luong@nimblehq.co>
2021-08-25 18:39:57 +0700

    [#5] Add multidex support on API 16

--- app/src/main/java/co/nimblehq/recentapps/thumbnailhiding/sample/App.kt
+++ app/src/main/java/co/nimblehq/recentapps/thumbnailhiding/sample/App.kt
@@ -1,6 +1,8 @@
 package co.nimblehq.recentapps.thumbnailhiding.sample
 
 import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
 import co.nimblehq.recentapps.thumbnailhiding.RecentAppsThumbnailHidingLifecycleTracker
 
 class App : Application() {
@@ -8,5 +10,10 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         registerActivityLifecycleCallbacks(RecentAppsThumbnailHidingLifecycleTracker())
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
     }
 }
