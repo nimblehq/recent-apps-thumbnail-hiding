192acf5230d1f0cc40c12a21971fc90506434916
Luong Vo (Lucas) <luong@nimblehq.co>
2021-08-29 11:33:19 +0700

    [#8] Add `enableSecureFlagOnDevicesWithCustomGestureNavigation` flag to have flexible config

--- lib/src/main/java/co/nimblehq/recentapps/thumbnailhiding/RecentAppsThumbnailHidingActivity.kt
+++ lib/src/main/java/co/nimblehq/recentapps/thumbnailhiding/RecentAppsThumbnailHidingActivity.kt
@@ -5,23 +5,32 @@ import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import co.nimblehq.recentapps.thumbnailhiding.navbar.NavigationBarObserver
+import co.nimblehq.recentapps.thumbnailhiding.navbar.NavigationBarObserver.isGestureEnabled
 
 abstract class RecentAppsThumbnailHidingActivity : AppCompatActivity(),
     RecentAppsThumbnailHidingListener {
-
-    protected open val enableSecureFlagOnLowApiDevices: Boolean = false
 
     /**
      * HardwareKeyWatcher doesn't work on API 25 or lowers,
      * allow to use FLAG_SECURE instead to hide app thumbnail.
      */
-    val isSecureFlagEnabled: Boolean
+    open val enableSecureFlagOnLowApiDevices: Boolean = false
+
+    /**
+     * HardwareKeyWatcher doesn't work on some Xiaomi or Huawei devices with custom gesture navigation enabled,
+     * allow to use FLAG_SECURE instead to hide app thumbnail.
+     */
+    open val enableSecureFlagOnDevicesWithCustomGestureNavigation: Boolean = false
+
+    private val isSecureFlagOnLowApiDevicesEnabled: Boolean
         get() = enableSecureFlagOnLowApiDevices && Build.VERSION.SDK_INT < Build.VERSION_CODES.O
+
+    private val isSecureFlagOnDevicesWithCustomGestureNavigationEnabled: Boolean
+        get() = enableSecureFlagOnDevicesWithCustomGestureNavigation && isGestureEnabled(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (isSecureFlagEnabled || NavigationBarObserver.isNavigationGestureEnabled(this)) {
+        if (isSecureFlagOnLowApiDevicesEnabled || isSecureFlagOnDevicesWithCustomGestureNavigationEnabled) {
             enableSecureFlag(true)
         }
     }
@@ -38,4 +47,9 @@ fun Activity.enableSecureFlag(enable: Boolean) {
             WindowManager.LayoutParams.FLAG_SECURE
         )
     }
+}
+
+fun Activity.isSecureFlagEnabled(): Boolean {
+    val flags = window.attributes.flags
+    return (flags and WindowManager.LayoutParams.FLAG_SECURE) != 0
 }
